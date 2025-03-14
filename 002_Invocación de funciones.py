6e452a8d7a4ca790f911a31347088b0f7ddf5fe4
vianneyaguila <a22310212@live.ceti.mx>
2025-03-13 22:03:16 -0600

    Práctica 2

---
+++ 002_Invocación de funciones.py
@@ -0,0 +1,4 @@
+def despedir(nombre): #Se define la función con el nomnre de despedir y un argumento de nombre
+    print(f"Adios,{nombre}!") 
+
+    despedir("Mauricio") #Se llama a la función con un argumento específico 
