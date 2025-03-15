fe2edfba10aba677558267c7fdea54a530f7fcb1
vianneyaguila <a22310212@live.ceti.mx>
2025-03-14 21:22:05 -0600

    Práctica 2

---
+++ 002_Literales de Python.py
@@ -0,0 +1,14 @@
+#Literales: los datos en si mismos
+print("2")
+print(2)
+
+#Números enteros
+print(0o123)
+print(0x123)
+
+#Flotadores
+print(0.000000000000000000001)
+
+#Cuerdas
+print('No me gusta el aguacate \"wakis"')
+print("I don\'t like avocado")
