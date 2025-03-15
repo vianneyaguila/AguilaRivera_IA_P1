e6409cc4a129e273b38f8d8c5a7fe3c78c972fa0
vianneyaguila <a22310212@live.ceti.mx>
2025-03-14 23:46:06 -0600

    Práctica 4

--- 004_Variables.py
+++ 004_Variables.py
@@ -1,23 +1,54 @@
 #Cómo crear una variable
-var = 1
-print(var)
+var1 = 1
+print(var1)
 
 #Cómo utilizar una variable
+var2 = 2
 saldo_de_cuenta = 500.00
 nombre_del_cliente = 'Vianney Aguila'
-print (var, saldo_de_cuenta , nombre_del_cliente )
-print (var)
+print (var2, saldo_de_cuenta , nombre_del_cliente )
+print (var2)
 
-var = "3.8.5"
-print("Python version: " + var)
+var3 = "3.8.5"
+print("Python version: " + var3)
 
 #Como asiganar un nuevo valor a una variable ya existente
-print (var)
-var = var + 1
-print (var) 
+var4 = 3
+print (var4)
+var4 = var4 + 1
+print (var4) 
 
-var = 100
-var = 200 + 300
-print(var)
+var5 = 100
+var5 = 200 + 300
+print(var5)
 
 #Hasta este punto del código se trabaja de 2.4.1 a 2.4.5
+
+#Resolución de problemas matematicos
+a = 3
+b = 4
+c = (a ** 2 + b ** 2) ** 0.5
+print("c = ", c)
+
+#Variables de LAB
+John = 3
+Mary = 5 
+Adan = 6 
+print(John , Mary , Adan)
+total_apples = John + Mary + Adan
+print(total_apples)
+
+Misael = 5
+Vianney = 5
+print( Misael , Vianney)
+total_tacos = Misael + Vianney
+print(total_tacos)
+print(((Misael + 1) + (Vianney + 1 )) / 2)
+
+# Variables LAB: un convertidor simple
+Kilometros = 12.25
+Millas = 7.38
+millas_a_kilometros = Millas * 1.61
+kilometros_a_millas = Kilometros / 1.61
+print( Millas , "Millas", round(millas_a_kilometros , 2) , "Kilometros")
+print(Kilometros , "Kilometros", round(kilometros_a_millas, 2) ,"Millas")
