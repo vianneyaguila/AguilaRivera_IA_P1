358f0e63049ef65829d6b9ac83c3e735eb709084
vianneyaguila <a22310212@live.ceti.mx>
2025-03-14 23:16:55 -0600

    Práctica 4

---
+++ 004_Variables.py
@@ -0,0 +1,23 @@
+#Cómo crear una variable
+var = 1
+print(var)
+
+#Cómo utilizar una variable
+saldo_de_cuenta = 500.00
+nombre_del_cliente = 'Vianney Aguila'
+print (var, saldo_de_cuenta , nombre_del_cliente )
+print (var)
+
+var = "3.8.5"
+print("Python version: " + var)
+
+#Como asiganar un nuevo valor a una variable ya existente
+print (var)
+var = var + 1
+print (var) 
+
+var = 100
+var = 200 + 300
+print(var)
+
+#Hasta este punto del código se trabaja de 2.4.1 a 2.4.5
