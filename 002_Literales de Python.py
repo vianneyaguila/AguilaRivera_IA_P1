e4de6415b323156e2153cc09f3c45f6a689120f5
vianneyaguila <a22310212@live.ceti.mx>
2025-03-14 22:02:36 -0600

    Práctica 2

--- 002_Literales de Python.py
+++ 002_Literales de Python.py
@@ -1,14 +1,22 @@
 #Literales: los datos en si mismos
-print("2")
+print("2") #Notaciones que representan valores fijos
 print(2)
 
-#Números enteros
+#Números enteros: números enteros sin componente fraccionario.
 print(0o123)
 print(0x123)
 
-#Flotadores
+
+#Flotadores: números que pueden contener un componente fraccionario.
 print(0.000000000000000000001)
 
 #Cuerdas
 print('No me gusta el aguacate \"wakis"')
 print("I don\'t like avocado")
+
+#Valores booleanos: son objetos constante, utilizados para represedntar valores de verdad.
+print(True > False)
+print(True < False)
+
+#Literales de Python LAB: cadenas
+print("\"I'm\" \n\"\"learnig\"\" \n\"\"\"Python\"\"\" ")
