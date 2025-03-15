a2af7cb056955839a4f758cc0200ccab5ecdca7d
vianneyaguila <a22310212@live.ceti.mx>
2025-03-14 22:50:47 -0600

    Práctica 3

---
+++ 003_Operadores_herramientas de manipulación.py
@@ -0,0 +1,30 @@
+#Python como calculadora
+print(2+2)
+
+#Operadores Básicos
+# Exponente
+print(2 ** 4)
+print(2 ** 4)
+print(2. ** 4)
+print(2. ** 4.)
+#Multiplicación
+print(2 * 4)
+print(2 * 4)
+print(2. * 4)
+print(2. * 4.)
+#Divición
+print(2 / 4)
+print(2 / 4)
+print(2. / 4)
+print(2. / 4.)
+#Divición de enteros
+print(2 // 4.5)
+print(2 // 4.5)
+print(-4. // 2)
+print(6. // -4.)
+#Resto (módulo)
+print( 14 % 4)
+print(12 % 4.5)
+#Suma
+print(15+23)
+print(-25 - 10)
