fbabf21d26a362de61bfdcba777541d2ae5119b7
vianneyaguila <a22310212@live.ceti.mx>
2025-03-15 18:56:17 -0600

    Práctica 5

---
+++ 005_Interacción con el usuario.py
@@ -0,0 +1,13 @@
+#La función input() con un argumento 
+print("Tell me anything...")
+anything = input
+print ("Hmm..." , anything, "...Really?")
+
+# El resultado de la función input()
+anything1 = input("Tell me anything")
+print("Hmm...", anything1, "...Really?")
+
+#Conversión de tipos
+Cualquier_cosa = float( input ("Ingrese un número:"))
+algo = Cualquier_cosa ** 2
+print(Cualquier_cosa, "Elevdo a la potencia de 2 es:", algo) 
